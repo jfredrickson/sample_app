e5260763c43a56cb9f812575c1f4c9d5339412ff
Jeff Fredrickson <jfredrickson@gmail.com>
2010-06-10 19:31:57 -0400

    Finished first cut of the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,14 +13,54 @@
 require 'spec_helper'
 
 describe User do
+  
   before(:each) do
-    @valid_attributes = {
-      :name => "value for name",
-      :email => "value for email"
+    @attr = {
+      :name => "Example User",
+      :email => "user@example.com"
     }
   end
 
   it "should create a new instance given valid attributes" do
-    User.create!(@valid_attributes)
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid email addresses" do
+    valid_addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    valid_addresses.each do |address|
+      valid_user_email = User.new(@attr.merge(:email => address))
+      valid_user_email.should be_valid
+    end
+  end
+  
+  it "should reject invalid email addresses" do
+    invalid_addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    invalid_addresses.each do |address|
+      invalid_user_email = User.new(@attr.merge(:email => address))
+      invalid_user_email.should_not be_valid
+    end
+  end
+  
+  it "should reject duplicate email addresses" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    duplicate_email_user = User.new(@attr)
+    duplicate_email_user.should_not be_valid
   end
 end
