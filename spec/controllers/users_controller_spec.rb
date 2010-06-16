4941bdb50afbb2d37a46ccaebf7751f96ff42130
Jeff Fredrickson <jfredrickson@gmail.com>
2010-06-15 20:11:43 -0400

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,13 +5,54 @@ describe UsersController do
   
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_tag("title", /Sign up/)
+    end
+  end
+  
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+        @user = Factory.build(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(false)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_tag("title", /Sign up/i)
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New User", :email => "newuser@example.com", :password => "foobar",
+                  :password_confirmation => "foobar" }
+        @user = Factory(:user, @attr)
+        User.stub!(:new).and_return(@user)
+        @user.should_receive(:save).and_return(true)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
     end
   end
   
@@ -22,24 +63,23 @@ describe UsersController do
     end
     
     it "should be successful" do
-      get 'show', :id => @user
+      get :show, :id => @user
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'show', :id => @user
+      get :show, :id => @user
       response.should have_tag("title", /#{@user.name}/)
     end
     
     it "should include the user's name" do
-      get 'show', :id => @user
+      get :show, :id => @user
       response.should have_tag("h2", /#{@user.name}/)
     end
     
     it "should have a profile image" do
-      get 'show', :id => @user
+      get :show, :id => @user
       response.should have_tag("h2>img", :class => "gravatar")
     end
-    
   end
 end
